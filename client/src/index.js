303501cf57a6d39af38a770a6060c706e38a1997
NithinThuML <thummanithin@gmail.com>
2023-08-23 12:38:55 +0530

    analytics

--- client/src/index.js
+++ client/src/index.js
@@ -17,6 +17,9 @@ import {
 } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import { PersistGate } from "redux-persist/integration/react";
+import { inject } from '@vercel/analytics';
+ 
+inject();
 
 const persistConfig = { key: "root", storage, version: 1 };
 const persistedReducer = persistReducer(persistConfig, authReducer);
